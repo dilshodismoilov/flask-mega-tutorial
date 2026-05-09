47223a19bb301ba3a869a6b812cf5f57b302acef
Dilshodjon Ismoilov <dilshodismoilov1711@gmail.com>
2026-05-09 14:01:51 +0500

    part 5 done

--- app/models.py
+++ app/models.py
@@ -3,8 +3,11 @@ from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
 
@@ -15,6 +18,12 @@ class User(db.Model):
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author'
     )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) # type: ignore
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -30,3 +39,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
