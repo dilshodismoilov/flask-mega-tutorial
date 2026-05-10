093001f13f726102e69aa01a4da777181683d0b0
Dilshodjon Ismoilov <dilshodismoilov1711@gmail.com>
2026-05-10 12:20:51 +0500

    part 9 done

--- app/forms.py
+++ app/forms.py
@@ -57,3 +57,10 @@ class EditProfileForm(FlaskForm):
 
 class EmtpyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)
+    ])
+    submit = SubmitField('Submit')
