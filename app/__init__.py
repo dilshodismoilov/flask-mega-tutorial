4f18ecce777c2a4ccdbbd47909adda0c89c28e0b
Dilshodjon Ismoilov <dilshodismoilov1711@gmail.com>
2026-05-10 15:20:24 +0500

    part 9 done

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 login = LoginManager(app)
 login.login_view = 'login' #type: ignore
 
